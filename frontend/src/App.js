5a4868adb3ab79e1dd7f08d7e6fb839d8277b863
Sandali Disara <sandalidissanayake123@gmail.com>
2023-04-10 16:28:34 +0530

    create table to view member details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import ViewMemebers from './components/ViewMemebers';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ViewMemebers/>
     </div>
   );
 }
---
+++ frontend/src/components/ViewMemebers.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function ViewMemebers() {
+  return (
+    <div className="conatiner">
+      <table border="1">
+        <tr>
+          <th>Name</th>
+          <th>Gender</th>
+          <th>Telephone</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Date of Birth</th>
+        </tr>
+        <tr>
+
+        </tr>
+      </table>
+    </div>
+  )
+}
